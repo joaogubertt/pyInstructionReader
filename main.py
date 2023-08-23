3a57b21e07024eb775a6c0901900cc35386b0eca
João Pedro Gubertt <107253777+joaogubertt@users.noreply.github.com>
2023-08-23 09:15:21 -0300

    Final version

--- main.py
+++ main.py
@@ -3,19 +3,22 @@ import os
 instructionsLoaded = 0
 cyclesA = 0
 cyclesB= 0
-tClock = 0
+tClockA = 0
+tClockB = 0
 execTime = 0    #exec time = instructionsLoaded * clock
 count = 0
 cpiA = 0
 cpiB = 0
+performanceA = 0
+performanceB = 0
 dumpPath = 'dump.txt'
  #Remove spaces at the beginning and at the end of the string (in this case each line is a String)
 with open(dumpPath, 'r') as dumpFile:
     instructions = [instruction.strip() for instruction in dumpFile]
 
-tClock = int(input("Tempo de Clock: "))
-os.system('cls')
+
 print("Organização A:")
+tClockA = int(input("Tempo de Clock: "))
 rA = int(input("r: "))
 uA = int(input("u: "))
 jA = int(input("j: "))
@@ -25,8 +28,9 @@ arithmeticA = int(input("arithmetic: "))
 ecallA = int(input("ecall: "))
 loadA = int(input("load: "))
 os.system('cls')
-print(uA, jA, arithmeticA, rA, bA, sA, loadA, ecallA, cyclesA)
+
 print("Organização B:")
+tClockB = int(input("Tempo de Clock: "))
 rB = int(input("r: "))
 uB = int(input("u: "))
 jB = int(input("j: "))
@@ -35,6 +39,7 @@ sB = int(input("s: "))
 arithmeticB = int(input("arithmetic: "))
 ecallB = int(input("ecall: "))
 loadB = int(input("load: "))
+
 
 def cyclesCount(u, j, arithmetic, r, b, s, load, ecall):
     cycles = 0
@@ -60,8 +65,17 @@ def cyclesCount(u, j, arithmetic, r, b, s, load, ecall):
 cyclesA = cyclesCount(uA, jA, arithmeticA, rA, bA, sA, loadA, ecallA)
 cyclesB = cyclesCount(uB, jB, arithmeticB, rB, bB, sB, loadB, ecallB)
 instructionsLoaded = len(instructions)
-cpiA = cyclesA/instructionsLoaded
-cpiB = cyclesB/instructionsLoaded
-print(cyclesA)
-print(cyclesB)
-print(instructionsLoaded)
+cpiA = cyclesA/instructionsLoaded #average cycle per instruction A
+cpiB = cyclesB/instructionsLoaded #average cycle per instruction B
+print("Ciclos da CPU A: ", cyclesA)
+print("Ciclos da CPU B: ", cyclesB)
+performanceA = tClockA * cyclesA
+performanceB = tClockB * cyclesB
+if performanceA > performanceB:
+    dif = performanceA/performanceB
+    print("O Desempenho da organização B é ", dif, " vezes mais rápido que A")
+elif performanceB/performanceA:
+    dif = performanceB/performanceA
+    print("O Desempenho da organização A é ", dif, " vezes mais rápido que A")
+else:
+    print("Ambas organizações possuem o mesmo desempenho")  
